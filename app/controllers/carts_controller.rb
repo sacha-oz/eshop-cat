0eb0c76dcdd44e0a7afaca57654ecc5a10d6f4ad
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-11-28 17:20:47 +0100

    cart control

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,15 +6,16 @@ class CartsController < ApplicationController
 
     def create
         # définir quel user
-        @userid_cart = current_user.id
+        @userid_cart = current_user
         # définir items 
         @it = session[:item_id]
         #comme user has_one :cart et si le panier est vide on le crée! :)
         if current_user.cart == nil
             @cart = Cart.create(user_id: @userid_cart)
-        end
+        else
         #si le panier n'est pas vide, on ne rentre pas dans le premier if et on ajoute simplement au panier existant
-        @cart = CartItem.create(cart_id: current_user.cart.id, item_id: @it)
+        @cart = CartItem.add(@it, @it.price)
+        end
         #on sauvegarde le panier puis on redirige / ou message d'erreur 
         if @cart.save
             redirect_to  cart_path(current_user.cart.id)
@@ -25,19 +26,21 @@ class CartsController < ApplicationController
 
 
     def show
+        puts "="*80
+        puts params
+        puts "="*80
         #on vas chercher les paramètres pour définir la variable cart
-        @cart = Cart.find(params[:id])
-        # on définir les itmes du paniers
-        @cart_it = @cart.items
-        myitems = current_user.cart.items
+        if current_user.cart == nil
+            flash[:no_cart] = "Vous n'avez pas de panier en cours! veuillez ajouter des articles!"
+            redirect_to root_path
+        else
+            @cart = Cart.find(current_user.cart_id)
+            # on définir les items du paniers
+        @mycart = current_user.cart.item
         # initialisation d'un compteur a 0 pour l'addition du prix du panier
         total = 0
-        @all = myitems.length
-        session[:all] = @all
-        #calcul du prix du panier avec les items présents
-        myitems.each do |i|
-            total += i.price
-            @count = total
+        @mycart.map{ |item| total += item.price}
+        @count = [@mycart.length, total]
         end
     end
 
